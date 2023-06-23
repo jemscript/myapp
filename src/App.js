2801caa5d5060784ef208ae1e31900257bdeb4b5
jemscript <jem@one5commute.com>
2023-06-23 20:10:28 +0530

    done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          REACT JS UPLOADING TO NETLIFY
         </a>
       </header>
     </div>
